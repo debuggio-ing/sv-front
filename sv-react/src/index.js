5e919043653086cbda5fc3db539438a3083af582
Ulises Torrella <ulises.torrella@gmail.com>
2020-10-21 17:10:36 -0300

    remove actions from index

--- sv-react/src/index.js
+++ sv-react/src/index.js
@@ -5,7 +5,6 @@ import { Partida } from './partida/Partida';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from "redux"
 import partidaReducer from "./redux/reducers.js"
-import startGame from "./redux/actions.js"
 import { Provider } from "react-redux"
 
 const store = createStore(partidaReducer);
