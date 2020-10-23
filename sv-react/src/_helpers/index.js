c4efde224a320a15cefc94f73de38bd9285a72f6
Nicolás Ludueña <47897388+nicolasLuduena@users.noreply.github.com>
2020-10-23 14:28:32 -0300

    Svm 30 (#6)
    * initial commit
    
    * Corrected API address
    
    * Register connected to API

--- sv-react/src/_helpers/index.js
+++ sv-react/src/_helpers/index.js
@@ -1,4 +1,3 @@
 export * from './auth-header';
-export * from './fake-backend';
 export * from './handle-response';
 export * from './history';
--- sv-react/webpack.config.js
+++ sv-react/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://localhost:8080'
+            apiUrl: 'http://127.0.0.1:8000'
         })
     }
-}
+} 
