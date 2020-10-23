0189f1bf51b1735c86e9eaab58cc2272a4b991be
Nicolas Luduena <luduena.nicolas.victorio@gmail.com>
2020-10-23 14:16:09 -0300

    Register connected to API

--- sv-react/src/_services/authentication.service.js
+++ sv-react/src/_services/authentication.service.js
@@ -6,32 +6,29 @@ import { handleResponse } from '@/_helpers';
 const currentUserSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('currentUser')));
 
 export const authenticationService = {
-    login,
-    logout,
+    register,
     currentUser: currentUserSubject.asObservable(),
     get currentUserValue () { return currentUserSubject.value }
 };
 
-function login(email, password) {
+function register(username, email, password) {
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password })
+        body: JSON.stringify({username, email, password })
     };
 
-    return fetch(`${config.apiUrl}/users/authenticate`, requestOptions)
-        .then(handleResponse)
-        .then(user => {
-            // store user details and jwt token in local storage to keep user logged in between page refreshes
-            localStorage.setItem('currentUser', JSON.stringify(user));
-            currentUserSubject.next(user);
-
-            return user;
-        });
+    return fetch(`${config.apiUrl}/api/register`, requestOptions)
+    // handle errors
+    .then(handleResponse)
+    // store token if necessary
+    .then(user => {
+        console.log(user)
+    });
 }
-
 function logout() {
     // remove user from local storage to log user out
     localStorage.removeItem('currentUser');
     currentUserSubject.next(null);
 }
+
