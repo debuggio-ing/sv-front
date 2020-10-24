5d028f1288f4b6f9721400c07895ab4039973afc
Nicolas Luduena <luduena.nicolas.victorio@gmail.com>
2020-10-24 16:25:52 -0300

    SVM-46 Fixed token bug

--- sv-react/src/_helpers/auth-header.js
+++ sv-react/src/_helpers/auth-header.js
@@ -3,9 +3,12 @@ import { authenticationService } from '@/_services';
 export function authHeader() {
     // return authorization header with jwt token
     const currentUser = authenticationService.currentUserValue;
-    if (currentUser && currentUser.token) {
-        return { Authorization: `Bearer ${currentUser.token}` };
+    if (currentUser && currentUser.access_token) {
+        return { Authorization: `Bearer ${currentUser.access_token}` };
     } else {
         return {};
     }
 }
+
+
+
