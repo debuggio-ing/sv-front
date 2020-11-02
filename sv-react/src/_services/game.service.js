04b35cedd4fe5fceae0723d1dfe54c2edcd38311
Nicolas Luduena <luduena.nicolas.victorio@gmail.com>
2020-11-02 18:02:36 -0300

    SVM-75 Post proclamation card integrated with backend

--- sv-react/src/_services/game.service.js
+++ sv-react/src/_services/game.service.js
@@ -1,10 +1,14 @@
 import config from 'config';
-import { authHeader, handleResponse } from '@/_helpers';
+import {
+    authHeader,
+    handleResponse
+} from '@/_helpers';
 
 export const gameService = {
     vote,
     gameStatus,
     getDirProcCards,
+    postDirProcCards,
 };
 
 // Given 'Nox' or 'Lumos' and a gameId it sends a request to the API with the vote
@@ -17,9 +21,12 @@ function vote(chosen, gameId = 1) { //delete '= 1'
     console.log(gameId);
     const requestOptions = {
         method: 'POST',
-        headers: Object.assign(authHeader(),
-            { 'Content-Type': 'application/json' }),
-        body: JSON.stringify({ vote }),
+        headers: Object.assign(authHeader(), {
+            'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify({
+            vote
+        }),
     };
     return fetch(`${config.apiUrl}/api/games/` + gameId.toString() + `/vote/`, requestOptions)
         // handle errors
@@ -40,7 +47,7 @@ function gameStatus(gameId) {
         headers: authHeader(),
     };
     return fetch(`${config.apiUrl}/api/games/` + gameId.toString() + '/',
-        requestOptions).then(handleResponse)
+            requestOptions).then(handleResponse)
         .then(game => {
             return game;
         });
@@ -48,12 +55,27 @@ function gameStatus(gameId) {
 
 
 // Given a gameId it returns to the director the cards selected by the minsiter of the game
-function getDirProcCards(gameId){
+function getDirProcCards(gameId) {
     const requestOptions = {
         method: 'GET',
         headers: authHeader(),
     };
-    return fetch(`${config.apiUrl}/api/games/`+gameId.toString()+`/dir/proc/`, requestOptions).then(handleResponse).then(procCards=> {
+    return fetch(`${config.apiUrl}/api/games/` + gameId.toString() + `/dir/proc/`, requestOptions).then(handleResponse).then(procCards => {
+        return procCards;
+    })
+}
+
+
+// Given the cards to proclaim by the director, it returns true or false if the game is over or not
+function postDirProcCards(gameId, election) {
+    const requestOptions = {
+        method: 'POST',
+        headers: Object.assign(authHeader(), {
+            'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify(election),
+    };
+    return fetch(`${config.apiUrl}/api/games/` + gameId.toString() + `/dir/proc/`, requestOptions).then(handleResponse).then(procCards => {
         return procCards;
     })
 }
