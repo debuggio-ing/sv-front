d3db9ca820f5dbf9a246ee07b0c9e80f63f51f88
Nicolás Ludueña <47897388+nicolasLuduena@users.noreply.github.com>
2020-11-02 10:30:13 -0300

    Svm 79 Correct implementation of token refresh (#23)
    * SVM-80 fixing bug on starting game
    
    * solving conflicts <<3
    
    * interval shutdown on fail
    
    * SVM-79 Correct refresh of token implemented
    
    Co-authored-by: Ulises Torrella <ulises.torrella@gmail.com>

--- sv-react/src/_helpers/handle-response.js
+++ sv-react/src/_helpers/handle-response.js
@@ -1,16 +1,20 @@
-import { authenticationService } from '@/_services';
+import {
+    authenticationService
+} from '@/_services';
 
 export function handleResponse(response) {
     return response.text().then(text => {
         const data = text && JSON.parse(text);
+
+        if (data.detail == "Signature has expired") {
+            authenticationService.refreshToken();
+            return "Token refreshed";
+        }
+
         if (!response.ok) {
             if ([401, 403].indexOf(response.status) !== -1) {
                 // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
                 authenticationService.logout();
-            }
-            if (422 == response.status) {
-                authenticationService.refreshToken();
-                return "Token Refreshed";
             }
 
             const error = (data && data.message) || response.statusText;
@@ -20,5 +24,3 @@ export function handleResponse(response) {
         return data;
     });
 }
-
-
