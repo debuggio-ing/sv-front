b436313cec8cd1bb02b5cf7b705773e5f8a76505
Lautaro Lecumberry <lautarolecumberry@gmail.com>
2020-11-09 15:21:12 -0300

    SVM-105 add update (username) service

--- sv-react/src/Account/Update.jsx
+++ sv-react/src/Account/Update.jsx
@@ -21,7 +21,7 @@ function Update({ history }) {
 
     function onSubmit(fields, { setStatus, setSubmitting }) {
         setStatus();
-        accountService.update(user.id, fields)
+        accountService.update(fields.username)
             .then(() => {
                 alertService.success('Actualizado exitosamente', { keepAfterRouteChange: true });
                 history.push('.');
--- sv-react/src/_services/account.service.js
+++ sv-react/src/_services/account.service.js
@@ -14,12 +14,13 @@ export const accountService = {
     userInfo,
     currentData: currentDataSubject.asObservable(),
     logout,
+    update,
     get currentDataValue () {
         return currentDataSubject.value
     }
 }
 
-//returns {username, email, id} when possible
+// returns {username, email, id} when possible
 function userInfo() {
     const requestOptions = {
         method: 'GET',
@@ -39,4 +40,21 @@ function userInfo() {
 
 function logout (){
     currentDataSubject.next(null)
+}
+
+
+// update username with new value and returns an UserPublic schema.
+function update(username) {
+    const requestOptions = {
+        method: 'POST',
+        headers: authHeader(),
+    };
+
+    return fetch(`${config.apiUrl}/api/users/info/modify/?username=` + username,
+        requestOptions)
+        .then(handleResponse)
+        .then(userPublic => {
+            currentDataSubject.next(userPublic)
+            return userPublic;
+        })
 }
