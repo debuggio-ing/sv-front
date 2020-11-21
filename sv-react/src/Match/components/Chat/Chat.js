4f2a394d040532b28545aad9d2e3a1f113aebaaf
Lautaro Lecumberry <lautarolecumberry@gmail.com>
2020-11-20 21:10:38 -0300

    SVM-158 add working chat

--- sv-react/src/Match/components/Chat/Chat.js
+++ sv-react/src/Match/components/Chat/Chat.js
@@ -2,13 +2,9 @@ import React from 'react';
 import { TextField, List, ListItem, ListItemAvatar, Avatar, Button } from '@material-ui/core';
 
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import DirectionsIcon from '@material-ui/icons/Directions';
+import SendIcon from '@material-ui/icons/Send';
+import { Formik, Field, Form } from 'formik';
 
 const messages = [
     {
@@ -22,22 +18,13 @@ const messages = [
 ]
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    width: 400,
-  },
   input: {
     marginLeft: theme.spacing(1),
     flex: 1,
+    width: '85%',
   },
   iconButton: {
     padding: 10,
-  },
-  divider: {
-    height: 28,
-    margin: 4,
   },
 }));
 
@@ -56,17 +43,22 @@ function Chat({sendMessage, key}) {
           ))}
         </List>
 
-        <Paper component="form" className={classes.root}>
-          <InputBase
-            className={classes.input}
-            placeholder="send message"
-            inputProps={{ 'aria-label': 'search google maps' }}
-          />
-          <IconButton className={classes.iconButton} aria-label="search" onClick={() => sendMessage("buscanding")}>
-            <SearchIcon />
-          </IconButton>
-        </Paper>
-
+        <Formik
+          initialValues={{
+              mensaje: '',
+          }}
+          onSubmit={({ mensaje }) => {
+              sendMessage(mensaje);
+          }}
+          render={() => (
+              <Form>
+                <Field name="mensaje" type="text" className={classes.input} />
+                <IconButton className={classes.iconButton} type="submit">
+                  <SendIcon />
+                </IconButton>
+              </Form>
+          )}
+        />
         </div>);
 }
 
