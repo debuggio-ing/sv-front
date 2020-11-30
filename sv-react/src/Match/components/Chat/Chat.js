9a5e9412695c59706992e8aa47590e50dd58371a
aekyos <22533437+aekyos@users.noreply.github.com>
2020-11-29 22:18:55 -0300

    Svm 192 improve chat (#79)
    * SVM-192 add new chat
    
    * SVM-192 fix chat
    
    * SVM-192 add chat styles
    
    * SVM-192 remove chat header

--- sv-react/src/Match/components/Chat/Chat.js
+++ sv-react/src/Match/components/Chat/Chat.js
@@ -1,10 +1,10 @@
-import React, {useEffect, useRef} from 'react';
-import { TextField, List, ListItem, ListItemAvatar, Avatar, Button, Paper } from '@material-ui/core';
-
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import { MessageList, MessageGroup, Message, MessageText, ThemeProvider, Avatar } from '@livechat/ui-kit'
+import { lightDeathEaterViolet } from '@/Styles'
 import IconButton from '@material-ui/core/IconButton';
 import SendIcon from '@material-ui/icons/Send';
 import { Formik, Field, Form } from 'formik';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   input: {
@@ -17,56 +17,54 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function Chat({sendMessage, key, messages}) {
-    const classes = useStyles();
-
-    const messagesEndRef = useRef(null)
-    const messageScroll = useRef(null)
-
-    const scrollToBottom = () => {
-      var height = document.getElementById("messagesContainer").scrollHeight;
-
-      if (messageScroll.current.scrollTop > height - 500){
-        messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+const theme = {
+    MessageGroup:{
+      css:{
+        borderRadius: '15px 30px',
+        backgroundColor: lightDeathEaterViolet,
       }
     }
+}
 
-    useEffect(scrollToBottom, [messages]);
+function Chat({ sendMessage, messages }) {
+    const classes = useStyles();
 
     return (<div>
-        <Paper ref={messageScroll} id="messagesContainer" style={{maxHeight: 400, overflow: 'auto'}}>
-            {messages.map((message, index) => (
-                <ListItem key={index}>
-                <ListItemAvatar key={index}>
-                <Avatar alt={message.sender} src="/static/images/avatar/1.jpg" />
-                  </ListItemAvatar>
-                  {message.message}
-                </ListItem>
-            ))}
-            <div ref={messagesEndRef} />
-        </Paper>
+        <div style={{ width: '100%', height: 400 }}>
+            <ThemeProvider theme={theme}>
+                <MessageList active>
+                    {messages.map((message, index) => (
+                        <MessageGroup avatar='public/img/harry-potter.png'>
+                            <Message authorName={message.sender} key={index}>
+                                <MessageText key={index}>{message.message}</MessageText>
+                            </Message>
+                        </MessageGroup>
+                    ))}
+                </MessageList>
+            </ThemeProvider>
+        </div>
 
         <Formik
-          initialValues={{
-              message: '',
-          }}
-          onSubmit={(values, {resetForm}) => {
-              sendMessage(values.message);
-              resetForm({message: ''})
-          }}>
-            { formProps =>  (
-                <Form onSubmit={formProps.handleSubmit}>
-                  <Field name="message"
-                         type="text"
-                         className={classes.input}
-                         value={formProps.values.message}/>
-                  <IconButton className={classes.iconButton} type="submit">
-                    <SendIcon />
-                  </IconButton>
-                </Form>
-            )}
-        </Formik>
-        </div>);
+              initialValues={{
+                  message: '',
+              }}
+              onSubmit={(values, {resetForm}) => {
+                  sendMessage(values.message);
+                  resetForm({message: ''})
+              }}>
+                { formProps =>  (
+                    <Form onSubmit={formProps.handleSubmit}>
+                      <Field name="message"
+                             type="text"
+                             className={classes.input}
+                             value={formProps.values.message}/>
+                      <IconButton className={classes.iconButton} type="submit">
+                        <SendIcon />
+                      </IconButton>
+                    </Form>
+                )}
+            </Formik>
+    </div>);
 }
 
 export default Chat;
